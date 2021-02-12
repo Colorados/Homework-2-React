cf72a68149fa81f2c1144fb166f8981f0dd6ef91
Colorados <fall3nleaf@gmail.com>
2021-02-12 16:15:10 +0600

    First commit (not really)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Button, Container} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        counter: 0,
+    };
+
+    increaseCounterByOne = () => {
+        this.setState({counter: this.state.counter + 1});
+    };
+    decreaseCounterByOne = () => {
+        this.setState({
+            counter: this.state.counter - 1,
+        });
+    };
+    increaseCounterByFive = () => {
+        this.setState({
+            counter: this.state.counter + 5,
+        });
+    };
+    decreaseCounterByFive = () => {
+        this.setState({
+            counter: this.state.counter - 5,
+        });
+    };
+    multiplyByTen = () => {
+        this.setState({
+            counter: this.state.counter * 10,
+        });
+    };
+    divideByTen = () => {
+        this.setState({
+            counter: this.state.counter / 10,
+        });
+    };
+    render() {
+        if (this.state.counter <= 0) {
+            this.state.counter = 0;
+        }
+        return (
+            <Container className="d-flex flex-lg-wrap justify-content-around text-center mt-2 d-flex justify-content-between">
+                <div className="col-12 mt-2">
+                    {this.state.counter}
+                </div>
+                <Button className="col-4 m-2" color="info" onClick={this.increaseCounterByOne}>
+                    increase by 1
+                </Button>
+                <Button className="col-4 m-2" color="danger" onClick={this.decreaseCounterByOne}>
+                    decrease by 1
+                </Button>
+                <Button className="col-4 m-2" color="info" onClick={this.increaseCounterByFive}>
+                    increase by 5
+                </Button>
+                <Button className="col-4 m-2" color="danger" onClick={this.decreaseCounterByFive}>
+                    decrease by 5
+                </Button>
+                <Button className="col-4 m-2" color="info" onClick={this.multiplyByTen}>
+                    multiply by 10
+                </Button>
+                <Button className="col-4 m-2" color="danger" onClick={this.divideByTen}>
+                    divide by 10
+                </Button>
+            </Container>
+        );
+    }
 }
 
 export default App;
